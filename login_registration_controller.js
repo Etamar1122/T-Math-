9e90279d19728f77287bb7f87cad489acfcefc2b
Etamar <etamar1122@gmail.com>
2021-12-23 15:39:20 +0200

    login registration system

--- login_registration_controller.js
+++ login_registration_controller.js
@@ -3,28 +3,39 @@ const registration_form = $("#registration_form");
 registration_form.on('submit', (e) =>{
     e.preventDefault();
     console.log("works fine2")
-    register_user()
+    try{
+        register_user()
+    }catch(error){
+        console.log(error);
+    }
+    
 })
+    async function register_user(){
+        const firstName = $('#first_name').val();
+        const lastName = $('#last_name').val();
+        const username = firstName;
+        const email = $('#email').val();
+        const password = ($('#password').val())
+        const role = "student";
+        const score = '0';
 
-async function register_user(){
-    const first_name = $('#first_name').val();
-    const last_name = $('#last_name').val();
-    const email = $('#email').val();
-    const password = ($('#password').val())
-    console.log("first_name", first_name)
-    console.log("works fine")
-
-    const result = await fetch('http://localhost:5500/api/register', {
-        method: "POST",
-        headers: {
-            'Content-Type':'application/json'
-        },
-        body: JSON.stringify({
-            first_name,
-            last_name,
-            email,
-            password
-        })
-    }).then(res => res)
-    console.log(result)
-}
+        try{
+            const result = await $.ajax({
+                url:'http://localhost:4000/users/register',
+                method: "POST",
+                contentType:'application/json',
+                data: JSON.stringify({
+                    firstName,
+                    lastName,
+                    username,
+                    // email,
+                    password,
+                    role,
+                    score
+                })
+            })
+            .then(res =>res.body)
+        } catch (error){
+            alert(JSON.parse(error.responseText).message);
+        }
+    }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const server = express();
 const path = require('path');
 const Sequelize  = require('sequelize');
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 
 const sequelize = new Sequelize('mysql://root:@localhost/t-math')
 
@@ -16,28 +16,28 @@ server.use(express.urlencoded({ extended: false }))
 const users_db = 'users'
 
 // check documentation for user initiation.
-server.post('/api/register', async (req, res) => {
-    const {email, password, first_name, last_name} = req.body
-    const encryptedpass =await bcrypt.hash(password,10);
+// server.post('/api/register', async (req, res) => {
+//     const {email, password, first_name, last_name} = req.body
+//     const encryptedpass =await bcrypt.hash(password,10);
   
-    try{  
-        const existsallready = await sequelize
-        .query(`SELECT * FROM users WHERE 'email' LIKE 'asdfasdf@a2sdfasdf.com'`)
-        console.log(existsallready)
-        if(await existsallready[0] == ''){
-            const result = await sequelize
-            .query(`INSERT INTO ${users_db} VALUES(null, '${email}' , '${encryptedpass}'  , '${first_name}' ,'${last_name}')`)
-            console.log(result);
-            return res.json({status: 'success', user_id: result[0]});
-        }else{
-            return res.json({ status: 'error_email_exists' })
-        }
+//     try{  
+//         const existsallready = await sequelize
+//         .query(`SELECT * FROM users WHERE 'email' LIKE 'asdfasdf@a2sdfasdf.com'`)
+//         console.log(existsallready)
+//         if(await existsallready[0] == ''){
+//             const result = await sequelize
+//             .query(`INSERT INTO ${users_db} VALUES(null, '${email}' , '${encryptedpass}'  , '${first_name}' ,'${last_name}')`)
+//             console.log(result);
+//             return res.json({status: 'success', user_id: result[0]});
+//         }else{
+//             return res.json({ status: 'error_email_exists' })
+//         }
 
-    }catch (error){
-        console.log(error)
-        return res.json({ status: 'error' })
-    }
-})
+//     }catch (error){
+//         console.log(error)
+//         return res.json({ status: 'error' })
+//     }
+// })
 
 // Bcrypt decrypt for user login user for later.
 // "password"; usually stored in the database in the user's row.
