d19939f5cac272311e11a83537cf49e25a667ab8
Etamar1122 <etamar1122@gmail.com>
2021-12-30 16:21:07 +0200

    db_functions

---
+++ db_functions.js
@@ -0,0 +1,17 @@
+async function get_users(){
+    try{
+        const result = await $.ajax({
+            url:'http://localhost:4000/users/',
+            method: "GET",
+             headers: {
+                "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjIsImlhdCI6MTY0MDY5MDMxNywiZXhwIjoxNjQxMjk1MTE3fQ.1gIf9c_Yw2Szkh3coNyhJSEuZ_d8HzBdjsDpizGgUHc"
+             },
+            contentType:'application/json',
+        }).then((res)=>{ return res});
+        return result
+    } catch (error){ 
+        alert(JSON.parse(error.responseText).message);
+    }
+}
+
+
